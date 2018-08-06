ef251b74bef3a49ec3674fda150f69e511a5affd
Lisa Hare <hare.lisa@gmail.com>
2018-08-06 15:34:38 -0700

    added put, post, and delete methods to catalog controller

--- ProductCatalogAPI/Controllers/CatalogController.cs
+++ ProductCatalogAPI/Controllers/CatalogController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using ProductCatalogAPI.Data;
 using ProductCatalogAPI.Domain;
+using ProductCatalogAPI.ViewModels;
 
 namespace ProductCatalogAPI.Controllers
 {
@@ -16,13 +18,18 @@ namespace ProductCatalogAPI.Controllers
     {
         //naming convention when it's a private variable to use an underscore
         private readonly CatalogContext _catalogContext;
+        private readonly IOptionsSnapshot<CatalogSettings> _settings; //IOptionsSnapshot will take a snapshot of when things are
 
         //constructor
-        public CatalogController(CatalogContext catalogContext)
+        public CatalogController(CatalogContext catalogContext, 
+            IOptionsSnapshot<CatalogSettings> settings)
+            
         {
             //the constructor will now know what database it is going to talk to and then set the
             //private variable so that everyone in the class has access
             _catalogContext = catalogContext;
+            _settings = settings;
+
         }
 
         [HttpGet]
@@ -65,10 +72,139 @@ namespace ProductCatalogAPI.Controllers
                               .ToListAsync();
 
             itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
 
-           
+            return Ok(model);
+        }
 
-            return Ok(itemsOnPage);
+        [HttpGet]
+        [Route("items/{id:int}")]
+        public async Task<IActionResult> GetItemById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+            var item = await _catalogContext.CatalogItems
+                .SingleOrDefaultAsync(c => c.Id == id);
+            if (item != null)
+            {
+                item.PictureUrl = item.PictureUrl
+                    .Replace("http://externalcatalogbaseurltobereplaced",
+                    _settings.Value.ExternalCatalogBaseUrl);
+                return Ok(item);
+            }
+            return NotFound();
+        }
+
+        //GET api/Catalog/items/withname/Wonder?pageSize=2&pageIndex=0
+        [HttpGet]
+        [Route("[action]/withname/{name:minlength(1)}")]
+        public async Task<IActionResult> Items(string name,
+            [FromQuery] int pageSize = 6,
+            [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                               .Where(c => c.Name.StartsWith(name))
+                              .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                              .Where(c => c.Name.StartsWith(name))
+                              .OrderBy(c => c.Name)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+
+            return Ok(model);
+        }
+
+        // GET api/Catalog/Items/type/1/brand/null[?pageSize=4&pageIndex=0]
+        [HttpGet]
+        [Route("[action]/type/{catalogTypeId}/brand/{catalogBrandId}")]
+        public async Task<IActionResult> Items(int? catalogTypeId,
+            int? catalogBrandId,
+            [FromQuery] int pageSize = 6,
+            [FromQuery] int pageIndex = 0)
+        {
+            var root = (IQueryable<CatalogItem>)_catalogContext.CatalogItems;
+
+            if (catalogTypeId.HasValue)
+            {
+                root = root.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+            if (catalogBrandId.HasValue)
+            {
+                root = root.Where(c => c.CatalogBrandId == catalogBrandId);
+            }
+
+            var totalItems = await root
+                              .LongCountAsync();
+            var itemsOnPage = await root
+
+                              .OrderBy(c => c.Name)
+                              .Skip(pageSize * pageIndex)
+                              .Take(pageSize)
+                              .ToListAsync();
+            itemsOnPage = ChangeUrlPlaceHolder(itemsOnPage);
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+
+            return Ok(model);
+
+        }
+        [HttpPost]
+        [Route("items")]
+        public async Task<IActionResult> CreateProduct(
+           [FromBody] CatalogItem product)
+        {
+            var item = new CatalogItem
+            {
+                CatalogBrandId = product.CatalogBrandId,
+                CatalogTypeId = product.CatalogTypeId,
+                Description = product.Description,
+                Name = product.Name,
+                //PictureFileName = product.PictureFileName,
+                Price = product.Price
+            };
+            _catalogContext.CatalogItems.Add(item);
+            await _catalogContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetItemById), new { id = item.Id });
+        }
+
+        [HttpPut]
+        [Route("items")]
+        public async Task<IActionResult> UpdateProduct(
+           [FromBody] CatalogItem productToUpdate)
+        {
+            var catalogItem = await _catalogContext.CatalogItems
+                              .SingleOrDefaultAsync
+                              (i => i.Id == productToUpdate.Id);
+            if (catalogItem == null)
+            {
+                return NotFound(new { Message = $"Item with id {productToUpdate.Id} not found." });
+            }
+            catalogItem = productToUpdate;
+            _catalogContext.CatalogItems.Update(catalogItem);
+            await _catalogContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetItemById), new { id = productToUpdate.Id });
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _catalogContext.CatalogItems
+                .SingleOrDefaultAsync(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+
+            }
+            _catalogContext.CatalogItems.Remove(product);
+            await _catalogContext.SaveChangesAsync();
+            return NoContent();
+
         }
 
         private List<CatalogItem> ChangeUrlPlaceHolder(List<CatalogItem> items)
@@ -77,7 +213,7 @@ namespace ProductCatalogAPI.Controllers
                 x => x.PictureUrl =
                 x.PictureUrl
                 .Replace("http://externalcatalogbaseurltobereplaced",
-                "http://localhost:5030"));
+                _settings.Value.ExternalCatalogBaseUrl));
             return items;
         }
     }
