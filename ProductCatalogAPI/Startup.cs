b8ea031519e301ec7f33ffa601b1109738a68882
Lisa Hare <hare.lisa@gmail.com>
2018-08-08 11:20:34 -0700

    dockerized project

--- ProductCatalogAPI/Startup.cs
+++ ProductCatalogAPI/Startup.cs
@@ -26,9 +26,18 @@ namespace ProductCatalogAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CatalogSettings>(Configuration);
+
             services.AddMvc();
+
+            //  string connectionString = 
+            var server = Configuration["DatabaseServer"];
+            var database = Configuration["DatabaseName"];
+            var user = Configuration["DatabaseUser"];
+            var password = Configuration["DatabaseUserPassword"];
+            var connectionString = String.Format("Server={0};Database={1};User={2};Password={3};", server, database, user, password);
+
             services.AddDbContext<CatalogContext>(
-                options => options.UseSqlServer(Configuration["ConnectionString"]));
+                options => options.UseSqlServer(connectionString));
 
             // Add framework services.
             services.AddSwaggerGen(options =>
